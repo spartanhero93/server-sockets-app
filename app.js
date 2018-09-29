7d7982008ec46bb71c3436b98b1eefa424e23137
Luis Lopez <name.luislopez@gmail.com>
2018-09-29 14:50:50 -0400

    added DB config

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const io = require('socket.io')(server)
 const SocketManager = require('./sockets')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const config = require('./config/config')
 
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true }, () => {
-  console.log('mongoose is connected')
-})
+mongoose.connect(
+  process.env.DB_URI || config.db,
+  { useNewUrlParser: true },
+  () => console.log('mongoose is connected')
+)
 
 const port = process.env.PORT || 3000
 server.listen(port, () => console.log(`Listening on port ${port}`))
 
+/** Start the socket server and listen for request from the client-socket */
 io.origins('*:*')
 io.on('connection', client => {
   console.log('Client connected')
